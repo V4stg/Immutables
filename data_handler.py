7255ec2634081ead62b068588c36b519af6392bd
Lukács Bence <lukacsb@gmail.com>
2018-02-14 14:07:04 +0100

    added get all incomes SQL request to data handler

--- data_handler.py
+++ data_handler.py
@@ -8,4 +8,12 @@ def all_users(cursor):
     return users
 
 
-print(all_users())
+@database_common.connection_handler
+def get_all_incomes(cursor, session):
+    cursor.execute("""
+                    SELECT incomes.name, inc_category_id, price, submission_time, comment, inc_categories.name AS inc_category FROM incomes
+                    INNER JOIN inc_categories ON inc_categories.id = incomes.inc_category_id
+                    WHERE user_id = %(user_id)s
+                    ORDER BY submission_time DESC
+                    """, session)
+    return cursor.fetchall()
