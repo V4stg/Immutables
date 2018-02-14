cb25a22d42c5015ed8d208ac969f4967613efaef
Lukács Bence <lukacsb@gmail.com>
2018-02-14 14:06:14 +0100

    added show incomes to server

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import data_handler
 app = Flask(__name__)
 
 
-
+@app.route('/incomes')
+def show_incomes():
+    data = data_handler.get_all_incomes(session)
+    keys = ['name', 'inc_category', 'price', 'submission_time', 'comment']
+    head = {'name': 'Name', 'inc_category': 'Income category', 'price': 'Price', 'submission_time': 'Date', 'comment': 'Comment'}
+    table = {'keys': keys, 'head': head, 'body': data}
+    return render_template('list.html', table=table)
