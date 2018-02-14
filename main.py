8e1093a391aa09e85e3ae94e5dd7f62164517c06
szilardjanosi <raned00@gmail.com>
2018-02-14 14:19:07 +0100

    import adjusted

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, url_for
-import data_handler
+from flask import Flask, render_template, request, url_for, redirect, session
+import data_handler, hash_handler
 from datetime import datetime
 
 app = Flask(__name__)
