0ba1badc06b75426bbd49c879aa7d76cbd3d3cff
szilardjanosi <raned00@gmail.com>
2018-02-15 15:27:24 +0100

    added buttons

--- main.py
+++ main.py
@@ -71,24 +71,30 @@ def registration():
 
 @app.route('/add-expense', methods=['GET', 'POST'])
 def add_expense():
-    if request.method == 'POST':
-        expense = request.form.to_dict()
-        expense.update({'user_id': session['user_id'], 'submission_time': datetime.now()})
-        data_handler.add_expense(expense)
-        return redirect('/')
-    options = data_handler.get_exp_categories()
-    return render_template('add_expense.html', options=options)
+    if 'user_id' in session:
+        if request.method == 'POST':
+            expense = request.form.to_dict()
+            expense.update({'user_id': session['user_id'], 'submission_time': datetime.now()})
+            data_handler.add_expense(expense)
+            return redirect('/expenses')
+        options = data_handler.get_exp_categories()
+        return render_template('add_expense.html', options=options)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/add-income', methods=['GET', 'POST'])
 def add_income():
-    if request.method == 'POST':
-        income = request.form.to_dict()
-        income.update({'user_id': session['user_id'], 'submission_time': datetime.now()})
-        data_handler.add_income(income)
-        return redirect('/')
-    options = data_handler.get_inc_categories()
-    return render_template('add_income.html', options=options)
+    if 'user_id' in session:
+        if request.method == 'POST':
+            income = request.form.to_dict()
+            income.update({'user_id': session['user_id'], 'submission_time': datetime.now()})
+            data_handler.add_income(income)
+            return redirect('/incomes')
+        options = data_handler.get_inc_categories()
+        return render_template('add_income.html', options=options)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/delete-expense/<int:expense_id>')
